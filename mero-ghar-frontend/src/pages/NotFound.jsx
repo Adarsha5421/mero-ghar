427e8b6ba4278570a663e12310d244cf46427366
Adarsha5421 <adarsha.sigdel12@gmail.com>
2025-07-17 21:54:58 +0545

    not found page created

--- mero-ghar-frontend/src/pages/NotFound.jsx
+++ mero-ghar-frontend/src/pages/NotFound.jsx
@@ -1,3 +1,3 @@
 export default function NotFound() {
-    return <h1 className="text-center mt-10 text-red-600 text-xl">404 - Page Not Found</h1>;
+    return <h1 className="text-center mt-12 text-red-600 text-xl">404 - Page Not Found</h1>;
 }
